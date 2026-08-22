39e8a546d8c983b5c6b050b509df692a95faaaed
Emad <gethino@gmail.com>
2026-08-22 18:16:02 +0200

    chore: rebrand footer links and cleanup redundant template whitespace

--- resources/scripts/components/InvoicePublicPage.vue
+++ resources/scripts/components/InvoicePublicPage.vue
@@ -4,33 +4,12 @@ I
     <div class="bg-linear-to-r from-primary-500 to-primary-400 h-5"></div>
 
     <div
-      class="
-        relative
-        p-6
-        pb-28
-        px-4
-        md:px-6
-        w-full
-        md:w-auto md:max-w-xl
-        mx-auto
-      "
+      class="relative p-6 pb-28 px-4 md:px-6 w-full md:w-auto md:max-w-xl mx-auto"
     >
       <BasePageHeader :title="pageTitle || ''">
         <template #actions>
           <div
-            class="
-              flex flex-col
-              md:flex-row
-              absolute
-              md:relative
-              bottom-2
-              left-0
-              px-4
-              md:px-0
-              w-full
-              md:space-x-4 md:space-y-0
-              space-y-2
-            "
+            class="flex flex-col md:flex-row absolute md:relative bottom-2 left-0 px-4 md:px-0 w-full md:space-x-4 md:space-y-0 space-y-2"
           >
             <a :href="shareableLink" target="_blank" class="block w-full">
               <BaseButton
@@ -64,7 +43,7 @@ I
         class="flex items-center justify-center mt-4 text-gray-500 font-normal"
       >
         Powered by
-        <a href="https://invoiceshelf.com" target="_blank">
+        <a href="https://www.facebook.com/SparxSolutions218/" target="_blank">
           <img :src="getLogo()" class="h-4 ml-1 mb-1" />
         </a>
       </div>
--- resources/scripts/customer/layouts/partials/TheSiteFooter.vue
+++ resources/scripts/customer/layouts/partials/TheSiteFooter.vue
@@ -1,27 +1,13 @@
 <template>
   <footer
-    class="
-      fixed
-      bottom-0
-      flex
-      items-center
-      justify-end
-      w-full
-      h-10
-      py-2
-      pe-8
-      text-sm
-      font-normal
-      text-gray-700
-      bg-white
-    "
+    class="fixed bottom-0 flex items-center justify-end w-full h-10 py-2 pe-8 text-sm font-normal text-gray-700 bg-white"
   >
     Powered by
     <a
-      href="http://bytefury.com/"
+      href="https://www.facebook.com/SparxSolutions218/"
       target="_blank"
       class="ps-1 font-normal text-gray-900"
-      >Bytefury
+      >Sparx Solutions
     </a>
   </footer>
 </template>
